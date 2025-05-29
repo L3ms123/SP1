446f0661ec12299dce2d7c9849a16e11a788c2e0
discomanfulanito <pablonecpas@gmail.com>
2025-05-29 21:23:44 +0200

    connecting

--- server.py
+++ server.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import Dict, Any
 from find_translator import *
 import os
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 data_path = os.path.join(script_dir, ".", "Data")
@@ -32,10 +39,34 @@ class Task(BaseModel):
 
 @app.post("/get-translators")
 async def getTranslators(task: Task):
-    get_top_translators_for_task(task_input_dict=task,
+    processed_task= {
+        'TASK_TYPE': task.taskType,
+        'SOURCE_LANG': task.sourceLanguage,
+        'TARGET_LANG': task.targetLanguage,
+        'TASK_ID': 32134,
+
+        'MANUFACTURER': task.manufacturer,
+        'MANUFACTURER_INDUSTRY': task.manufacturerIndustry,
+        'MANUFACTURER_SUBINDUSTRY': task.manufacturerSubindustry,
+        'SELLING_HOURLY_PRICE': task.pricePerHour,
+        'MIN_QUALITY': task.minQuality,
+        'ASSIGNED':  12
+    }
+    top = get_top_translators_for_task(task_input_dict=processed_task,
                                  data_df=data_df,
                                  transl_cost_pairs_df=transl_cost_pairs_df,
                                  clients_df=clients_df,
                                  schedules_df=schedules_df,
                                  top_k=4)
+    print("top: ", top)
+    print(f"source: {task.sourceLanguage}")
+    print(f"target: {task.targetLanguage}")
+    print(f"type: {task.taskType}")
+    print(f"manufacturer: {task.manufacturer}")
+    print(f"industry: {task.manufacturerIndustry}")
+    print(f"subindustry: {task.manufacturerSubindustry}")
+    print(f"quality> {task.minQuality}")
+    print(f"wildcard: {task.wildcard}")
+    print(f"price/h: {task.pricePerHour}")
+    return top
       
